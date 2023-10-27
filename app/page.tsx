9011af839c54e663b813c13bb5869dbd03872578
djmaulana <53201103+djmaulana@users.noreply.github.com>
2023-10-27 15:47:06 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
                 <Link 
                   href='/invitation'
                 >
-                  <button onClick={handelMusic} className="bg-[#474747] my-[50px] py-2 px-4 md:hidden block rounded-full w-40 text-center text-white text-[18px]">
+                  <button onClick={handelMusic} className="bg-[#474747] mt-[40px] mb-[70px] py-2 px-4 md:hidden block rounded-full w-40 text-center text-white text-[18px]">
                     Buka Undangan
                   </button>
                 </Link>
